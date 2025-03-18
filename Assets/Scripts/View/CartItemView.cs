9ee394bd545dd19e4f82abb76b2a96237ed8ffe1
Savelii Bondarenko <savabondarenko2016@gmail.com>
2025-03-18 19:29:42 +0200

    Прибрав зайві public

--- Assets/Scripts/View/CartItemView.cs
+++ Assets/Scripts/View/CartItemView.cs
@@ -4,37 +4,34 @@ using UnityEngine.UI;
 
 public class CartItemView : MonoBehaviour
 {
-    public Image productImage;
-    public TMP_Text productName;
-    public TMP_Text productQuantity;
-    public Button removeButton;
-    public Button addOneButton; 
-    public Button removeOneButton; 
-    public Button removeAllButton; 
+    [SerializeField] private Image productImage;
+    [SerializeField] private TMP_Text productName;
+    [SerializeField] private TMP_Text productQuantity;
+    [SerializeField] private Button removeButton;
+    [SerializeField] private Button addOneButton;
+    [SerializeField] private Button removeOneButton;
+    [SerializeField] private Button removeAllButton;
 
-    private Product currentProduct;
-    private CartView cartView;
+    private Product _currentProduct;
 
     public void Setup(Product product, int quantity, CartView cartView)
     {
-        currentProduct = product;
-        this.cartView = cartView;
-
+        _currentProduct = product;
         productName.text = product.Name;
         productQuantity.text = $"x{quantity}";
         productImage.sprite = product.ProductImage;
 
         // Setup button listeners
         addOneButton.onClick.RemoveAllListeners();
-        addOneButton.onClick.AddListener(() => cartView.UpdateProductQuantity(currentProduct, quantity + 1));
+        addOneButton.onClick.AddListener(() => cartView.UpdateProductQuantity(_currentProduct, quantity + 1));
 
         removeOneButton.onClick.RemoveAllListeners();
-        removeOneButton.onClick.AddListener(() => cartView.UpdateProductQuantity(currentProduct, Mathf.Max(0, quantity - 1)));
+        removeOneButton.onClick.AddListener(() => cartView.UpdateProductQuantity(_currentProduct, Mathf.Max(0, quantity - 1)));
 
         removeAllButton.onClick.RemoveAllListeners();
-        removeAllButton.onClick.AddListener(() => cartView.RemoveProductFromCart(currentProduct));
+        removeAllButton.onClick.AddListener(() => cartView.RemoveProductFromCart(_currentProduct));
 
         removeButton.onClick.RemoveAllListeners();
-        removeButton.onClick.AddListener(() => cartView.RemoveProductFromCart(currentProduct));
+        removeButton.onClick.AddListener(() => cartView.RemoveProductFromCart(_currentProduct));
     }
 }
